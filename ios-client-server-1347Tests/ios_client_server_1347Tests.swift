36e96f4fdb880ba9b1c57d9360f6fda1fb8f41cd
Oleg <ktuuuu@gmail.com>
2021-07-06 13:15:28 +0400

    Домашнее задание 1

---
+++ ios-client-server-1347Tests/ios_client_server_1347Tests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_client_server_1347Tests.swift
+//  ios-client-server-1347Tests
+//
+//  Created by Олег on 06.07.2021.
+//
+
+import XCTest
+@testable import ios_client_server_1347
+
+class ios_client_server_1347Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
